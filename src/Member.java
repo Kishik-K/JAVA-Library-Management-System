052b56460abc1cd5772489f3cf7c8cc1252eba14
Kishik-K <kishikkhajuria03@gmail.com>
2026-08-03 23:27:33 +0530

    added Member class methods

--- src/Member.java
+++ src/Member.java
@@ -4,4 +4,35 @@ public class Member {
     private String name;
     private String memberId;
     private ArrayList<Book> borrowedBooks;
+
+    public Member(String name, String memberId) {
+        this.name = name;
+        this.memberId = memberId;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (ID: " + memberId + ") - Borrowed: " + borrowedBooks.size() + " book(s)";
+    }
 }
