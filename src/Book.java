6d58c7a216a02b267e2ec9c0370a0f0b6117a7c4
Kishik-K <kishikkhajuria03@gmail.com>
2026-08-03 23:16:59 +0530

    completed Book class

--- src/Book.java
+++ src/Book.java
@@ -11,4 +11,28 @@ public class Book {
         this.isAvailable = true;
     }
 
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString(){
+        return title + " by " + author + " (ISBN: " + isbn + ")" + (isAvailable ? " [Available]" : " [Checked out]");
+    }
 }
