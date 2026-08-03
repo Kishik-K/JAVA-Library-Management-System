a8a34bc10f3d191dcde089db7b79a55d5134e97f
Kishik-K <kishikkhajuria03@gmail.com>
2026-08-03 23:40:22 +0530

    Added Imports & starting methods

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Library {
+    private HashMap<String, Book> books;
+    private ArrayList<Member> members;
+
+    public Library(){
+        this.books = new HashMap<>();
+        this.members = new ArrayList<>();
+    }
+
+    // ------------BOOK MANAGEMENT-----------
+
+    public boolean addBook(Book book){
+        if (books.containsKey(book.getIsbn())){
+            System.out.println("Book already exists.");
+            return false;
+        }
+        books.put(book.getIsbn(), book);
+        return true;
+    }
 }
