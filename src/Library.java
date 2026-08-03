2628f193b06edc446b172b52958aac407d813627
Kishik-K <kishikkhajuria03@gmail.com>
2026-08-03 23:45:22 +0530

    Finished Book-Management methods.

--- src/Library.java
+++ src/Library.java
@@ -20,4 +20,20 @@ public class Library {
         books.put(book.getIsbn(), book);
         return true;
     }
+    public boolean removeBook(String isbn){
+        return books.remove(isbn) != null;
+    }
+    public void listBooks(){
+        if(books.isEmpty()){
+            System.out.println("No books in the library");
+            return;
+        }
+        for (Book b: books.values()){
+            System.out.println(b);
+        }
+    }
+    public Book searchBook(String isbn){
+        return books.get(isbn);
+    }
+
 }
